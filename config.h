630c6e136c4d8bd550d33c7ce4429a9cd88a6b80
hamid <hamid@hamidout.xyz>
2021-01-13 18:02:43 +0100

    increased font size

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=13", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#7e7474";
 static char normfgcolor[]           = "#bbbbbb";
